c4e9dfb49cb74eba5fb999649ec13f3d6189738d
久田 歩 <pykgg476@gmail.com>
2014-08-09 15:54:22 +0900

    sass等の環境設定完了

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "common/css"
+sass_dir = "common/scss"
+image_dir = "imageFile"
+javascript_dir = "common/js"
+output_style = :compact; #:compresssed
+line_comments = false;
