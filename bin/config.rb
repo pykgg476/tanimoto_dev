435ab1c56d9be60657cf76ac7c3357c2e88b6dde
久田 歩 <pykgg476@gmail.com>
2014-09-29 01:35:02 +0900

    圧縮したり、タイトル小さくしたり

--- bin/config.rb
+++ bin/config.rb
@@ -3,5 +3,5 @@ css_dir = "common/css"
 sass_dir = "common/scss"
 images_dir = "imageFile"
 javascript_dir = "common/js"
-output_style = :compact; #:compresssed
-line_comments = false;
+output_style = :compressed
+# line_comments = false
