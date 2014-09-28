a172a74c4e3a32fbfb41fd8c8bb0fae7d526f55f
久田 歩 <pykgg476@gmail.com>
2014-09-28 15:42:14 +0900

    いろいろ直した。

--- common/js/underPage.js
+++ common/js/underPage.js
@@ -19,16 +19,8 @@ $(function(){
 
 	// サイドナビ固定
 	if($('#sideNaviArea').length){
-		var cH = $('.contentsArea').offset().top;
-		var sideNavi = $('#sideNaviArea');
-		var sideNaviPos = $('#globalHeader').height() + 25;
-		$(window).scroll(function(){
-			if($(this).scrollTop() > cH+40){
-				sideNavi.css({position: 'fixed', top:sideNaviPos});
-			}else{
-				sideNavi.css({position: 'relative', top:0});
-			}
-		});
+		sideNaviPos();
+		$(window).scroll(function(){ sideNaviPos(); });
 	}
 			
   
@@ -39,3 +31,21 @@ $(function(){
  Function
 ----------------------------------------*/
 
+var sideNaviPos = function(){
+	var ct = $('.contentsArea').offset().top;
+	var ch = $('.contentsArea').height();
+	var sn = $('#sideNaviArea');
+	var f = $('#globalFooter');
+	var snPos = $('#globalHeader').height() + 25;
+	var snh = sn.height();
+	var ds = $(document).scrollTop();
+	var ft = f.offset().top;
+	var fh = f.height();
+	if(ds>ct+ch-snh){
+			sn.css({position: 'absolute',top:ct+ch-snh});
+		}else if(ds>ct+40){
+			sn.css({position: 'fixed', top:snPos});
+		}else{
+			sn.css({position: 'relative', top:0});
+		}
+}
