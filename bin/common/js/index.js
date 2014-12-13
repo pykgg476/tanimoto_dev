f2fa8144aa91e4e8af5216981f47b41ae389eb39
久田 歩 <pykgg476@gmail.com>
2014-12-13 10:23:41 +0900

    トップページの修正

--- bin/common/js/index.js
+++ bin/common/js/index.js
@@ -15,11 +15,8 @@ $(function(){
       $('#topPanelArea').find('.base').fadeIn(800);
     },1500);
     setTimeout(function(){
-      $('#topPanelArea').find('.catch').fadeIn(800);
+      $('#topPanelArea').find('.symbolSet').fadeIn(800);
     },2000);
-    setTimeout(function(){
-      $('#topPanelArea').find('.logo').fadeIn(800);
-    },2700);
   });
 
   // パネルのサイズ
@@ -38,7 +35,10 @@ $(function(){
     $(this).find('img').stop(true,true).animate({width: '-=20px',left: '+=10px', top: '+=10px'});
   });
   
-
+  // ヘッダーロゴ差し替え
+  var thp = $('#globalHeader').offset().top + $('#globalHeader').height();
+  headerLogo(thp);
+  $(window).scroll(function(){ headerLogo(thp); });
 
 });
 	
@@ -46,6 +46,20 @@ $(function(){
 /*----------------------------------------
  Function
 ----------------------------------------*/
+
+var headerLogo = function(thp){
+  var head = $('#globalHeader').find('.logo');
+  var ds = $(document).scrollTop();
+  if($('#topPanelArea').length){
+    var ph = $('#topPanelArea').height();
+    ds = ds - ph + thp;
+  }
+  if(thp < ds){
+    head.find('.noSymbol').addClass('is_disable').siblings('.symbol').removeClass('is_disable');
+  }else if(thp >= ds){
+    head.find('.symbol').addClass('is_disable').siblings('.noSymbol').removeClass('is_disable');
+  }
+}
 
 var panelSize = function(){
 	var ww = $(window).width();
