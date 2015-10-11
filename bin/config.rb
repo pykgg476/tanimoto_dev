325a1c3f0e6f34d58a5b929c1202c6a175d96aaa
久田 歩 <pykgg476@gmail.com>
2015-10-11 21:54:42 +0900

    準備

--- bin/config.rb
+++ bin/config.rb
@@ -1,3 +1,5 @@
+Encoding.default_external = 'UTF-8'
+
 http_path = "/"
 css_dir = "common/css"
 sass_dir = "common/scss"
@@ -5,3 +7,18 @@ images_dir = "imageFile"
 javascript_dir = "common/js"
 output_style = :compressed
 # line_comments = false
+
+on_sprite_saved do |filename|
+  if File.exists?(filename)
+    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+  end
+end
+
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+    end
+  end
+end
